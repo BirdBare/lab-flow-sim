7309cd037b4d4266ddf7e0975de4c6a2f8c90962
JJ-BradleyBare <bradleybare@gmail.com>
2026-01-28 18:58:08 -0500

    update models to use uuid id

--- src/orm/device/models/function.py
+++ src/orm/device/models/function.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 from .device import Device
-
+import uuid
 
 class Function(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     device = models.ForeignKey(to=Device, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=255)
+
+    category = models.CharField(max_length=255,choices=(("Spatial","Spatial"),("Material","Material"),("Informational","Informational")))
 
     execution_time_formula = models.CharField(max_length=255)
 

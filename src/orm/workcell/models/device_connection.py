4c2d72b677526464397801386c6c26296ea2ff25
JJ-BradleyBare <bradleybare@gmail.com>
2026-02-11 15:02:47 -0500

    distance to connection

--- src/orm/workcell/models/device_connection.py
+++ src/orm/workcell/models/device_connection.py
@@ -10,3 +10,5 @@ class DeviceConnection(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     assigned_devices:models.ManyToManyField[AssignedDevice,DeviceConnection] = models.ManyToManyField(to=AssignedDevice)
+
+    distance = models.PositiveIntegerField()

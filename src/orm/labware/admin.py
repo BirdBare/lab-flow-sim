b8cf19e0a88a5a424dbba80376660655c22b732a
JJ-BradleyBare <bradleybare@gmail.com>
2026-02-11 23:41:06 -0500

    initial commit

---
+++ src/orm/labware/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Labware
+
+admin.site.register(Labware)
---
+++ src/orm/labware/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LabwareConfig(AppConfig):
+    name = "orm.labware"

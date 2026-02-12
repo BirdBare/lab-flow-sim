8a1ddb5a1de6262f15f23c5bb056b0d92f22f13c
JJ-BradleyBare <bradleybare@gmail.com>
2026-02-11 22:52:42 -0500

    add process

--- src/orm/workcell/admin.py
+++ src/orm/workcell/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Workcell,AssignedDevice,DeviceConnection
+
+from .models import AssignedDevice, DeviceConnection, Process, Workcell
 
 admin.site.register(Workcell)
 admin.site.register(AssignedDevice)
 admin.site.register(DeviceConnection)
+admin.site.register(Process)
--- src/orm/workcell/models/__init__.py
+++ src/orm/workcell/models/__init__.py
@@ -1,3 +1,4 @@
 from .assigned_device import AssignedDevice
 from .device_connection import DeviceConnection
 from .workcell import Workcell
+from .process import Process

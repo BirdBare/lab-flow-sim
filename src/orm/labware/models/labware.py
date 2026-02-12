4eaf6628947eb3664876ecaf8971cc345fafd207
JJ-BradleyBare <bradleybare@gmail.com>
2026-02-11 23:43:16 -0500

    associate with workcell

--- src/orm/labware/models/labware.py
+++ src/orm/labware/models/labware.py
@@ -2,9 +2,13 @@ import uuid
 
 from django.db import models
 
+from orm.workcell.models import Workcell
+
 
 class Labware(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    workcell = models.ForeignKey(to=Workcell, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=255)
 
